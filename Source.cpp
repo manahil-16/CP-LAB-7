f9e37e1ec418bd0bc89aaf58a03a110a79690be8
manahil-16 <146338432+manahil-16@users.noreply.github.com>
2023-11-02 16:40:57 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+int main()
+{
+	int books,issuedate,returndate,fine,choice;
+	cout << "Enter books You want to borrow:"
+	cin >> books;
+	cout << "Enter issuedate:"
+	cin >> issuedate;
+	cout << "Enter returndate:"
+	cin >> returndate;
+
+	cout << "1.Book Borrowed";
+	cout<<"2.No Book Borrowd"
+
+}
